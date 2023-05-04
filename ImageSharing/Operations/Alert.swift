251a9bd02ac06b5ab3a93abba3c42a593f33d085
muratyildirimm <muratyildirimm495@gmail.com>
2023-05-05 00:59:04 +0300

    Firebase Authentication is done!!!

---
+++ ImageSharing/Operations/Alert.swift
@@ -0,0 +1,13 @@
+//
+//  Alert.swift
+//  ImageSharing
+//
+//  Created by Murat Yıldırım on 4.05.2023.
+//
+
+import Foundation
+import UIKit
+
+
+
+
--- ImageSharing/Scene/SettingsViewController.swift
+++ ImageSharing/Scene/SettingsViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SettingsViewController: UIViewController {
 
@@ -16,7 +17,13 @@ class SettingsViewController: UIViewController {
     
 
   @IBAction func exitButton(_ sender: Any) {
-    performSegue(withIdentifier: "toViewController", sender: nil)
+    do {
+      try Auth.auth().signOut()
+      performSegue(withIdentifier: "toViewController", sender: nil)
+    } catch {
+      print(error.localizedDescription)
+    }
+ 
   }
   
 
