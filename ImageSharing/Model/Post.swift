7a233e63cfe5da5bf93b95f9ee10f13e81509b8e
muratyildirimm <muratyildirimm495@gmail.com>
2023-05-07 21:26:42 +0300

    App is done actually!!! All that's left is to edit the code.

---
+++ ImageSharing/Model/Post.swift
@@ -0,0 +1,15 @@
+//
+//  Post.swift
+//  ImageSharing
+//
+//  Created by Murat Yıldırım on 7.05.2023.
+//
+
+import Foundation
+
+struct Post {
+  var email: String
+  var comment: String
+  var imageUrl: String
+}
+
--- ImageSharing/Scene/FeedViewController.swift
+++ ImageSharing/Scene/FeedViewController.swift
@@ -1,15 +1,17 @@
 
 import UIKit
 import Firebase
+import SDWebImage
 
 class FeedViewController: UIViewController {
 
   // MARK: IBOutlet
   @IBOutlet weak var tableView: UITableView!
   // MARK: Variables
-  var emailList = [String]()
-  var commentList = [String]()
-  var imageList = [String]()
+  //var emailList = [String]()
+  //var commentList = [String]()
+  //var imageList = [String]()
+  var postList = [Post]()
   
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,15 +21,25 @@ class FeedViewController: UIViewController {
 
   func getDataFromFirestore() {
     let db = Firestore.firestore()
-    let dbRef = db.collection("Post")
+    // db.collection("Post).whereField("email", isEqualTo: "murat123" yaparsan sadece o hesabın paylaşımlarını getirir
+    let dbRef = db.collection("Post").order(by: "date", descending: true)
     dbRef.addSnapshotListener { snapshot, error in
       if error != nil {
         self.popupAlert(title: "Title", message: "Message", actionStyle: .default)
       } else {
         if snapshot?.isEmpty != true && snapshot != nil {
+         // self.imageList.removeAll(keepingCapacity: false)
+         // self.emailList.removeAll(keepingCapacity: false)
+         // self.commentList.removeAll(keepingCapacity: false)
+          self.postList.removeAll(keepingCapacity: false)
           for document in snapshot!.documents {
            // let documentID = document.documentID
-            if let imageURL = document.get("imageurl") as? String {
+            guard let imageURL = document.get("imageurl") as? String else { return }
+            guard let comment = document.get("comment") as? String else { return }
+            guard let email = document.get("email") as? String else { return }
+            var post = Post(email: email, comment: comment, imageUrl: imageURL)
+            self.postList.append(post)
+           /* if let imageURL = document.get("imageurl") as? String {
               self.imageList.append(imageURL)
             }
             if let comment = document.get("comment") as? String {
@@ -35,7 +47,7 @@ class FeedViewController: UIViewController {
             }
             if let email = document.get("email") as? String {
               self.emailList.append(email)
-            }
+            }*/
            }
           self.tableView.reloadData()
         } else {
@@ -52,14 +64,14 @@ class FeedViewController: UIViewController {
 
 extension FeedViewController: UITableViewDataSource, UITableViewDelegate {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-     emailList.count ?? 0
+    postList.count
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell
-    cell.emailLabel.text = emailList[indexPath.row]
-    cell.commentLabel.text = commentList[indexPath.row]
-    cell.postImageView.image = UIImage(named: "select")
+    cell.emailLabel.text = postList[indexPath.row].email
+    cell.commentLabel.text = postList[indexPath.row].comment
+    cell.postImageView.sd_setImage(with: URL(string: postList[indexPath.row].imageUrl))
     return cell
   }
   
